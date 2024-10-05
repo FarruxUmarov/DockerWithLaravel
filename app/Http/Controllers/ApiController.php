1d6daac90f5a840000091d9126f7810871036332
Farrukh <adler4313@gmail.com>
2024-10-05 14:28:44 +0500

    MINOR: api id user

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class ApiController
 {
-    public function user()
+    public function user(Request $request, $id): \Illuminate\Http\JsonResponse
     {
-        $user = User::query()->create([
-            'name' => 'John Doe',
-            'email' => 'john@doe.com',
-            'password' => Hash::make('password')
-        ]);
+        $user = User::findOrFail($id);
 
         $token = $user->createToken('token')->plainTextToken;
         return response()->json(['token' => $token]);
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tokens/user', [\App\Http\Controllers\ApiController::class, 'user']);
+Route::get('/tokens/user/{id}', [\App\Http\Controllers\ApiController::class, 'user']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
